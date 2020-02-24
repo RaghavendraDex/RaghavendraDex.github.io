fb242d8b77c106524f4d9ba5ee8288e9797a44ac
Raghavendra <raghavendra.m@dexhigh.com>
2020-02-24 16:12:00 +0530

    changed to chat app

---
+++ precache-manifest.ba9fd97d91e401ee26b3a8dca048d282.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3bc066341c158f50daa00870e585e92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c58322b662dd9cb4e080",
+    "url": "/static/css/main.62a23081.chunk.css"
+  },
+  {
+    "revision": "495b3e38ae8f8f2521ca",
+    "url": "/static/js/2.61202074.chunk.js"
+  },
+  {
+    "revision": "8345da3dc262b15c21e065faca7dd545",
+    "url": "/static/js/2.61202074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c58322b662dd9cb4e080",
+    "url": "/static/js/main.22ce044a.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
